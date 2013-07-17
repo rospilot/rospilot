7105505ba2b7085eebaba67fba3f1e089c05365d
Christopher Berner <christopherberner@gmail.com>
2013-07-16 21:21:11 -0700

    Add Highcharts for accelerometer z-axis data

--- static/index.js
+++ static/index.js
@@ -35,6 +35,9 @@ var app = angular.module('rospilot', ['ngResource'])
   (function tick() {
       IMU.get({}, function(data) {
           $scope.data = data;
+          var series = $('#accel_z_chart').highcharts().series[0];
+          var x = (new Date()).getTime();
+          series.addPoint([x, data.accel.z], true, true);
           $timeout(tick, 1000);
       });
   })();
@@ -144,3 +147,65 @@ var app = angular.module('rospilot', ['ngResource'])
   })();
 });
 
+$(document).ready(function() {
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    var chart;
+    $('#accel_z_chart').highcharts({
+        chart: {
+            type: 'spline',
+            animation: Highcharts.svg, // don't animate in old IE
+            marginRight: 10,
+        },
+        title: {
+            text: 'Accelerometer'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150
+        },
+        yAxis: {
+            title: {
+                text: 'Value'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            formatter: function() {
+                    return '<b>'+ this.series.name +'</b><br/>'+
+                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                    Highcharts.numberFormat(this.y, 2);
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        exporting: {
+            enabled: false
+        },
+        series: [{
+            name: 'data',
+            data: (function() {
+                var data = [],
+                    time = (new Date()).getTime(),
+                    i;
+
+                for (i = -19; i <= 0; i++) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: 0
+                    });
+                }
+                return data;
+            })()
+        }]
+    });
+});
