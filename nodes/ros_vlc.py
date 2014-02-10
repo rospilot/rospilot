8b81d6386aa406407ee05f6ac31761480921bce7
bordicon <pandazero@gmail.com>
2014-02-09 16:51:24 -0800

    bug fixes for vlc node

--- nodes/ros_vlc.py
+++ nodes/ros_vlc.py
@@ -3,30 +3,31 @@
 import roslib; roslib.load_manifest('rospilot')
 import rospy
 import rospilot.msg
-import ros_vlc
+from vlc_server import server
 import std_srvs.srv
 
 class VlcNode:
-  def __init__(self):
-    # self.server = vlc_server.VlcServer(vbit_rate  = 256,
-    #                                    host       = "0.0.0.0",
-    #                                    port       = 8080
-    #                                    device     = "/dev/video0",
-    #                                    chroma     = "yuyv",
-    #                                    fps        = 30,
-    #                                    width      = 1280,
-    #                                    height     = 720)
-    self.recorder = vlc_server.VlcRecorder(mrl        = "http://0.0.0.0:8080",
-                                           dest_path  = "/tmp/ros_vid.mwv")
-    rospy.init_node("ros_vlc")
-    # rospy.Service('play_stream',  std_srvs.srv.Empty, self.handle_play_stream)
-    # rospy.Service('pause_stream', std_srvs.srv.Empty, self.handle_pause_stream)
-    # rospy.Service('stop_stream',  std_srvs.srv.Empty, self.handle_stop_stream)
+    def __init__(self):
+      # self.server = vlc_server.VlcServer(vbit_rate  = 256,
+      #                                    host       = "0.0.0.0",
+      #                                    port       = 8080
+      #                                    device     = "/dev/video0",
+      #                                    chroma     = "yuyv",
+      #                                    fps        = 30,
+      #                                    width      = 1280,
+      #                                    height     = 720)
+      self.recorder = server.VlcRecorder(
+          mrl        = "http://0.0.0.0:8080/stream?topic=/camera/image_raw",
+          dest_path  = "/tmp/ros_vid.mwv")
+      rospy.init_node("ros_vlc")
+      # rospy.Service('play_stream',  std_srvs.srv.Empty, self.handle_play_stream)
+      # rospy.Service('pause_stream', std_srvs.srv.Empty, self.handle_pause_stream)
+      # rospy.Service('stop_stream',  std_srvs.srv.Empty, self.handle_stop_stream)
 
-    rospy.Service('start_record', std_srvs.srv.Empty, self.handle_start_record)
-    rospy.Service('stop_record',  std_srvs.srv.Empty, self.handle_stop_record)
+      rospy.Service('start_record', std_srvs.srv.Empty, self.handle_start_record)
+      rospy.Service('stop_record',  std_srvs.srv.Empty, self.handle_stop_record)
 
-    def handle_start_record(self):
+    def handle_start_record(self, blah):
         self.recorder.start_record()
 
     def handle_stop_record(self, blah):
