64c2e25827d84add891c1dd185d2342dfa05ee23
Christopher Berner <christopherberner@gmail.com>
2015-01-05 04:45:12 -0800

    Remove old vlc recorder node

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['rospilot', 'vlc_server'],
+    packages=['rospilot'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy']
 )
