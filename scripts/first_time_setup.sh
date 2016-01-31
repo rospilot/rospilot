d67b51490171ee9665b375038a29834db283e55f
Christopher Berner <christopherberner@gmail.com>
2016-01-31 13:36:14 -0800

    Copy files to /etc as part of setup script instead of package install

--- scripts/first_time_setup.sh
+++ scripts/first_time_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Installing udev and init.d rules"
+sudo cp $(catkin_find --share rospilot share/etc/rospilot.rules) /etc/udev/rules.d/
+sudo cp $(catkin_find --share rospilot share/etc/init.d/rospilot) /etc/init.d/
+sudo chmod +x /etc/init.d/rospilot
 message="Enabling rospilot service. \
 Uninstall it with 'update-rc.d -f rospilot remove'"
 
